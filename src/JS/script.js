7d9ce593cecc643c5ff040ecda84c49adec61efe
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-20 10:38:25 +0530

    15 tasks

--- src/JS/script.js
+++ src/JS/script.js
@@ -18,4 +18,24 @@ $("#target1").prop("disabled", true);
 
 // 10.Change Text Inside an Element Using jQuery
 $("#target4").html("<em>#target4</em>");
-});  
+
+// 11.Remove an Element Using jQuery
+$("#target4").remove();
+
+// 12.Use appendTo to Move Elements with jQuery
+$("#target2").appendTo("#right-well");
+
+// 13.Clone an Element Using jQuery
+$("#target5").clone().appendTo("#left-well");
+
+// 14.Target the Parent of an Element Using jQuery
+$("#target1").parent().css("background-color", "red");
+
+// 15.Target the Children of an Element Using jQuery
+$("#right-well").children().css("color", "orange");
+
+  });
+  
+
+
+
