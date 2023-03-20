1d4ce655dc99bfc086d44732cd8fb71415cf774d
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-20 10:18:04 +0530

    5 tasks

---
+++ src/JS/script.js
@@ -0,0 +1,10 @@
+// 1.Learn How Script Tags and Document Ready Work
+$(document).ready(function() {
+    // 2.Target HTML Elements with Selectors Using jQuery
+    $("button").addClass("animated bounce");
+// 3.Target Elements by Class Using jQuery
+    $(".well").addClass("animated shake");
+    //4. Target Elements by id Using jQuery
+    $("#target3").addClass("animated fadeOut");
+    // 5.Delete Your jQuery Functions
+});
