a513db29f40c55ef320a940a3c3a917905684ec4
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-20 10:29:52 +0530

    10 tasks

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,5 +1,21 @@
-// 1.Learn How Script Tags and Document Ready Work
+// 6.Target the Same Element with Multiple jQuery Selectors
 $(document).ready(function() {
-    
-    // 5.Delete Your jQuery Functions
-});
+    $("button").addClass("animated");
+    $(".btn").addClass("shake");
+    $("#target1").addClass("btn-primary");
+      
+//  7.Remove Classes from an Element with jQuery
+$("button").addClass("animated bounce");
+$(".well").addClass("animated shake");
+$("#target3").addClass("animated fadeOut");
+$("button").removeClass("btn-default");
+
+//8. Change the CSS of an Element Using jQuery
+$("#target1").css("color", "red");
+
+// 9.Disable an Element Using jQuery
+$("#target1").prop("disabled", true);
+
+// 10.Change Text Inside an Element Using jQuery
+$("#target4").html("<em>#target4</em>");
+});  
